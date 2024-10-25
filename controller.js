866628259303df9f29ebd24040f609a47454f715
cris24tayler <cristian.tayler@alunoescolassociais.org.br>
2024-10-25 12:06:10 -0300

    termina o js

--- controller.js
+++ controller.js
@@ -1,11 +1,23 @@
 document.getElementById('conversorForm').addEventListener('submit', function(event){
+
+    event.preventDefault();
+    
     const valor = parseFloat(document.getElementById('valor').value);
     const moedaOrigem = document.getElementById('moedaOrigem').value;
     const moedaDestino = document.getElementById('moedaDestino').value;
-//definir taxa de canbio fixa 
+//definir taxa de cambio fixa 
     const exchangeRates = {
         USD: {EUR: 0.93, BRL: 5.71 },
         BRL: {EUR: 0.16, USD: 0.18 },
         EUR: {USD: 1.08, BRL: 6.16 }
     };
+    let valorConvertido;
+    if(moedaOrigem === moedaDestino){
+        valorConvertido = valor;
+    } else{
+        valorConvertido = valor * exchangeRates[moedaOrigem][moedaDestino];
+    }
+
+    let resultado = document.getElementById('resultado');
+        resultado.textContent = `Resultado: ${valorConvertido.toFixed(2)} ${moedaDestino}`;
 });
